1a14b8a96e0747f02b3c3858647c63c81793d482
idan157001 <idanalbam1@gmail.com>
2023-10-02 12:54:00 +0300

    #Fixed all bugs  #Working Status

--- data.py
+++ data.py
@@ -3,10 +3,13 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 import firebase_admin
 from firebase_admin import credentials,db
-from typing import Dict
-from link_maker import runner
+from link_maker import make_link,make_img,save_imgs
 import re
 import random
+import string
+import os 
+import time
+
 class FireBase:
     _initialized = False
 
@@ -27,8 +30,24 @@ class FireBase:
             self.new_ref.child(node).set(data_dict)
         except ValueError as e:
             raise e
-
-    def check_links_at_db(self):
+    def upload_imgs_to_new_items(self):
+        # Need to use 
+        # Uploading Links of img for each item in /New_Items
+        nodes_to_update = []
+        urls = []
+        data = self.new_ref.get()
+        for (node,values) in data.items():
+            if 'img' not in values:
+                urls.append(values['link'])
+                nodes_to_update.append(node)
+        
+        results = make_img(urls)
+        
+        for node,img_link in zip(nodes_to_update,results):
+                self.new_ref.child(node).update({'img':img_link})
+        print(f'Uploaded {len(results)}\nNot Uploaded {len(nodes_to_update)}')
+        return(len(nodes_to_update))
+    def check_links_from_db(self):
          
         #Read Lines From Firebase check if link is good 
 
@@ -38,8 +57,7 @@ class FireBase:
             link = str(y['link'])
             if link.startswith('https://www.pandabuy.com/product') and  link.endswith('&inviteCode=ZGTWERRP3'):
                 counter+=1
-                print(x)
-        print(f'rows: {len(rows)}\ngood links {counter}')
+        print(f'bad rows: {len(rows)-counter}')
 
     def changing_new_items_links(self):
 
@@ -47,26 +65,29 @@ class FireBase:
         #This Function take some time 
 
         links = []
+        nodes_name_to_update = []
         node = self.new_ref.get()
 
         for node_name,values in node.items():
-            link = str(values.get('link'))  
+            link = values['link']  
             if not link.endswith('&inviteCode=ZGTWERRP3'):
+                nodes_name_to_update.append(node_name)
                 links.append(link)
                  
         print(f'Working On New Links {len(links)} ')
-        new_links = runner(links)
+        new_links = make_link(links)
 
-        for new_link,(node_name,values) in zip(new_links,node.items()):
-            if new_link == None or  not new_link.startswith('https://www.pandabuy.com/product'):
+        node = self.new_ref.get()
+        for new_link,node_name in zip(new_links,nodes_name_to_update):
+            if new_link is None or  not new_link.startswith('https://www.pandabuy.com/product'):
                 self.new_ref.child(node_name).delete()
                 continue
 
-            if not values['link'].endswith('ZGTWERRP3'):
-                self.new_ref.child(node_name).update({'link':new_link})
-        
+            
+            self.new_ref.child(node_name).update({'link':new_link})
+            print(new_link)
         print('New Linked Uploaded')
-
+        database.check_links_from_db()
 
     def upload_newItems_to_items(self):
         
@@ -76,10 +97,58 @@ class FireBase:
         new_items_data = self.new_ref.get()
         
         for name,item_data in new_items_data.items():
-             if len(item_data) == 3:
+             if len(item_data) == 4:
                   self.ref.child(name).set(item_data)
                   
         print('All Items has been moved')
+    def remove_bad_files(self):
+        #check if img in firebase if not remove it from folder
+        #fixing bug if happend
+        removed_count = 0
+        folder_path = fr"C:\Users\Idan's PC\OneDrive\Desktop\Pandabuy\pandabuy\Items"
+        items_data = self.ref.get()
+        s = set()
+        s1 = set()
+        for node,item_data in items_data.items():
+            s.add(item_data['file_name'])
+        
+
+        for item in os.listdir(folder_path):
+            if item not in s:
+                os.remove(f'{folder_path}\{item}')
+                removed_count+=1
+
+        print(removed_count)
+
+    def save_img_into_file(self):
+            #Saving Images to folder 
+            print('Saving Images to folder')
+            chars = string.ascii_letters + string.digits
+            urls = []
+            files_name = []
+            items_data = self.ref.get()
+            folder_path = fr"C:\Users\Idan's PC\OneDrive\Desktop\Pandabuy\pandabuy\Items"
+
+            for node,item_data in items_data.items():
+                file_id = ''.join(random.choice(chars) for _ in range(32))
+
+                while os.path.exists(os.path.join(folder_path, file_id)):
+                    file_id = ''.join(random.choice(chars) for _ in range(32))
+
+                if 'file_name' not in item_data:
+                    if '.png' in item_data['img']:
+                        self.ref.child(node).update({'file_name':f'{file_id}.png'})
+                    else:
+                        self.ref.child(node).update({'file_name':f'{file_id}.jpg'})
+
+                    items_data = self.ref.get()
+                    for node,item_data in items_data.items():
+                        urls.append(item_data['img'])
+                        files_name.append(item_data['file_name'])
+
+            save_imgs(urls,files_name)
+            print('Images Saved')
+
 
     def clear_new_items(self):
 
@@ -103,7 +172,8 @@ class Spread_sheet(FireBase):
         credentials = ServiceAccountCredentials.from_json_keyfile_name(self.credentials_file, scope)
         self.gc = gspread.authorize(credentials)
     
-    def clean_name_chars(self,name,link) -> str :
+    def clean_name_chars(self,name) -> str :
+
         #Making sure data is valid
 
         illegal_character_pattern = re.compile(r'[^a-zA-Z0-9]')
@@ -157,31 +227,43 @@ class Spread_sheet(FireBase):
             if any(item is None for item in (name, link, cny, dollar)):
                 continue
             else:
-                    name_valid = self.clean_name_chars(name, link)
-                    #name_valid = self.check_if_item_already_exits(name_valid, dollar)
-
+                    name_valid = self.clean_name_chars(name)
                     if name_valid and len(link) > 20:
-                        self.write_data(name_valid, {'link': link, 'cny': cny, 'dollar': dollar})
+                        new_ref_data = self.new_ref.get()
+                        if new_ref_data is not None:
+                            if name_valid not in new_ref_data.keys():
+                                self.write_data(name_valid, {'link': link, 'cny': cny, 'dollar': dollar})
+                        else:
+                            self.write_data(name_valid, {'link': link, 'cny': cny, 'dollar': dollar})
 
         print('Links Uploaded')
+    
 
 # SpreadSheet_url to upload
-#uploaded https://docs.google.com/spreadsheets/d/1j4of5mznL24dWs17rPrHuVWwSjzt76m1YkgIaPenOmQ/edit
-#need to upload https://docs.google.com/spreadsheets/d/1ffvM_fax9iuLEvqgIwprl7SMf2n0B8YaSLKXb843Wo4/htmlview?pru=AAABgurjdTc*cg2AYCugO3MpTM4qGBA62A#gid=0
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1ffvM_fax9iuLEvqgIwprl7SMf2n0B8YaSLKXb843Wo4/htmlview?pru=AAABgurjdTc*cg2AYCugO3MpTM4qGBA62A#gid=0'
-
 database = FireBase()
 item = Spread_sheet()
 
 
-def Upload_to_db():
-    item.write_spreadsheet_to_firebase(spreadsheet_url,20,50)
-    database.changing_new_items_links()
-
+def new_items_db():
+    item.write_spreadsheet_to_firebase(spreadsheet_url,8,450)
+    #database.changing_new_items_links()
     
-   
+    """not_uploaded = database.upload_imgs_to_new_items()# run solo
+    while not_uploaded > 0:
+        time.sleep(10)
+        not_uploaded = database.upload_imgs_to_new_items()
+    print('Finish')
+    """
+def items_db():
+    database.upload_newItems_to_items()
+    item.save_img_into_file()
 
+def main():
+    new_items_db()
+    #items_db()
 
-Upload_to_db()
-#database.check_links_at_db()
-#database.upload_newItems_to_items()
+main()
+
+#database.remove_bad_files()
+
--- link_maker.py
+++ link_maker.py
@@ -2,6 +2,9 @@ import requests
 import random
 import requests
 import asyncio
+from bs4 import BeautifulSoup as bs
+import urllib.parse
+
 My_Urls = []
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -15,16 +18,45 @@ user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0",
 ]
+async def grab_image(url:str) -> str:
 
+    # Send get request to url to get the image link
 
-async def make_affiliate_link(url):
-    #Making The Link and return it to Main
+    url = urllib.parse.unquote(url)
+    url = url.split('url=')[-1].split('&spider')[0]
+    
+    random_user_agent = random.choice(user_agents)
+    headers = {"User-Agent": random_user_agent}
     try:
-     
+        response = await asyncio.to_thread(requests.get, url, headers=headers,timeout=10)
+        if response.status_code == 200:
+            contect = response.content 
+            soup= bs(contect,'html.parser')
+            link_element = soup.find('link', {'rel': 'preload', 'as': 'image'})
+
+            if link_element:
+                href = link_element.get('href')
+                if href:
+                    print(href)
+                    img_link = href.split('?')[0]+ '?w=700&h=700&cp=1'
+                    return img_link
+    except:
+        pass
+async def grab_image_runner(urls) -> list:
+    # Create a list of tasks for make_item_image
+    tasks = [grab_image(url) for url in urls]
+    results = await asyncio.gather(*tasks)
+    return results
+    
+async def grab_link(url):
+
+    #Change Affilate code to mine 
+
+    try:
         random_user_agent = random.choice(user_agents)
         headers = {"User-Agent": random_user_agent}
 
-        response = await asyncio.to_thread(requests.get, url, headers=headers,timeout=3)
+        response = await asyncio.to_thread(requests.get, url, headers=headers,timeout=10)
         if response.status_code == 200:
             url = response.url
            
@@ -37,13 +69,15 @@ async def make_affiliate_link(url):
         
         
     except Exception as e:
-        raise e
+        pass
 
 # Create and run the asyncio event loop
-async def main(urls):
+async def grab_links_runner(urls):
+    
+    # activating grab_link() 
     if len(urls) <= 100:
         print(f'Uploading {len(urls)} links')
-        results = await asyncio.gather(*(make_affiliate_link(url) for url in urls))
+        results = await asyncio.gather(*(grab_link(url) for url in urls))
         return results
     else:
         results = []
@@ -52,17 +86,39 @@ async def main(urls):
         s,e = 0,100
         for i in range(chunks+1):
             if i == chunks:
-                results += await asyncio.gather(*(make_affiliate_link(url) for url in urls[chunks*100:url_len]))
+                results += await asyncio.gather(*(grab_link(url) for url in urls[chunks*100:url_len]))
                 
-            results += await asyncio.gather(*(make_affiliate_link(url) for url in urls[s:e]))
+            results += await asyncio.gather(*(grab_link(url) for url in urls[s:e]))
             s+=100
             e+=100
             print(f'chunk ({i+1}/{chunks+1})')
     
         return results
+    
+async def save_img_runner(urls,files_name):
+    tasks = [save_img(url,file_name) for url,file_name in zip(urls,files_name)]
+    results = await asyncio.gather(*tasks)
+    return results
 
+async def save_img(url,file_name):
+    file_path = fr"C:\Users\Idan's PC\OneDrive\Desktop\Pandabuy\pandabuy\Items\{file_name}"
+    random_user_agent = random.choice(user_agents)
+    headers = {"User-Agent": random_user_agent}
 
-def runner(urls):
-    return asyncio.run(main(urls))
+    response = await asyncio.to_thread(requests.get, url, headers=headers,timeout=10)
+    if response.status_code == 200:
+        with open(file_path,'wb') as file:
+            file.write(response.content)
+
+def make_link(urls):
+    return asyncio.run(grab_links_runner(urls))
+
+def make_img(urls,max_urls=30):
+    urls = urls[:max_urls]
+    return asyncio.run(grab_image_runner(urls))
+
+def save_imgs(urls,files_name):
+    return asyncio.run(save_img_runner(urls,files_name))
+
 
 
